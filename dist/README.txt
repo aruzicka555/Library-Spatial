0170ac48d4c96f4fb8bd0a0bb16f91b4e379006b
jdomingo <jdomingo@green-code.com>
2012-06-25 22:59:37 +0000

    Tagged version 1.0 of WinPkgTools setup program.

--- dist/README.txt
+++
@@ -1,9 +0,0 @@
-LANDIS Spatial Modeling Library version 0.82
-
-This release contains:
-
-  Landis.SpatialModeling.dll -- the library's API
-
-  Landis.SpatialModeling.CoreServices.dll -- the library's implementation
-
-  GDAL files for 32-bit Windows
--- src/api/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyDescription("Library API")]
-[assembly: AssemblyCopyright("Copyright 2010 Green Code LLC")]
-
-[assembly: InternalsVisibleTo("Landis.SpatialModeling.CoreServices")]
