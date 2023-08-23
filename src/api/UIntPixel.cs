6af4caf04c08d8b07f83ea5b7d94ac7328e5d120
Austen <austen.ruzicka@gmail.com>
2023-08-22 22:45:09 -0700

    Add Pixel Classes
    Move Pixel Classes to Spatial Library so they dont have to be in the extensions

---
+++ src/api/UIntPixel.cs
@@ -0,0 +1,15 @@
+﻿//  Copyright 2005-2010 Portland State University, University of Wisconsin
+//  Authors:  Srinivas S., Robert M. Scheller
+
+namespace Landis.SpatialModeling
+{
+    public class UIntPixel : Pixel
+    {
+        public Band<uint> MapCode = "The numeric code for each raster cell";
+
+        public UIntPixel()
+        {
+            SetBands(MapCode);
+        }
+    }
+}
