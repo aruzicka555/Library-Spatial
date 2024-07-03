b82423ee167b18a8d99179f71b5594463edbc1b1
Paul Schrum <paul.schrum@gmail.com>
2024-07-03 14:52:31 -0400

    Added files needed to implement Landis_GeoTiff.

---
+++ src/Landis_GeoTiff/IInputRaster.cs
@@ -0,0 +1,11 @@
+﻿namespace Landis_GeoTiff
+{
+    public interface IInputRaster<T> : System.IDisposable
+        where T : struct
+    {
+        T BufferPixel { get; }
+        T ReadBufferPixel();
+
+        Dimensions Dimensions { get; }
+    }
+}
---
+++ src/Landis_GeoTiff/RasterFactory.cs
@@ -0,0 +1,40 @@
+﻿using GeoTBelt.GeoTiff;
+using GeoTBelt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Landis_GeoTiff
+{
+    public static class RasterFactory
+    {
+        public static IInputRaster<T> OpenRaster<T>(string path)
+            where T : struct
+        {
+
+            var localRaster = (GeoTiffRaster<T>)Raster<T>.Load(path);
+
+            LandisRaster<T> workingRaster = new LandisRaster<T>();
+            workingRaster.theRaster = localRaster;
+
+            return (IInputRaster<T>)workingRaster;
+        }
+
+        public static IOutputRaster<T> CreateRaster<T>
+            (string path, Dimensions dimensions)
+            where T : struct
+        {
+            LandisRaster<T> returnRaster = new LandisRaster<T>();
+            returnRaster.theRaster =
+                GeoTiffRaster<T>.MakeNew(path,
+                dimensions.Rows, dimensions.Columns);
+
+            var v = returnRaster.theRaster;
+
+            returnRaster.Dimensions = dimensions;
+            return returnRaster;
+        }
+    }
+}
